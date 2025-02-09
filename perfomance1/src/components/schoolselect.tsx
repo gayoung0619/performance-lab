48416cb8adf1c336162383cab8f4ee10cd4569c2
gayoung <did-619@naver.com>
2025-02-10 00:00:48 +0900

    feat: add virtual scroll

--- perfomance1/src/components/schoolselect.tsx
+++ perfomance1/src/components/schoolselect.tsx
@@ -1,40 +1,80 @@
-import React, { useEffect, useState } from "react";
-import { Autocomplete, TextField, Box, Typography } from "@mui/material";
+import React, {useEffect, useRef, useState} from "react";
 import { useQuery } from "@tanstack/react-query";
 import {getSchoolList} from "../api/school";
 import styled from "styled-components";
+import { useWindowScroll, useWindowSize } from "react-use";
 
 const SchoolSelector = () => {
+  const itemHeight = 300;
+  const columnGap = 10;
+  const { height } = useWindowSize();
+  const { y } = useWindowScroll();
+
   const { data: schoolList, isLoading } = useQuery({
     queryKey: ["schoolList"],
     queryFn: () => getSchoolList(),
     staleTime: 24 * 60 * 60 * 1000,
   });
 
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [viewportY, setViewportY] = useState(0);
+
+  const relativeY = y - viewportY;
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      const viewportY = scrollRef.current.getBoundingClientRect().y;
+      setViewportY(viewportY);
+    }
+  }, []);
+
+  const containerHeight = (itemHeight + columnGap) * schoolList?.data.length
+
+  const startIndex = Math.max(
+      Math.floor(relativeY / (itemHeight + columnGap)), 0
+  )
+
+  const endIndex = Math.min(
+      Math.ceil(height / (itemHeight + columnGap) + startIndex), schoolList?.data.length
+  )
+
+  const visibleItem = schoolList?.data.slice(
+      Math.max(startIndex, 0),
+      Math.min(endIndex + 1, schoolList?.data.length)
+  )
+
+  const translateY = Math.max((itemHeight + columnGap) * startIndex, columnGap);
+
+
   return (
       <div>
         {isLoading ? (
             <p>Loading...</p>
         ) : (
-            <>
-              <SchoolList>
-                {schoolList?.data.map((ele, idx) => (
-                    <div>{ele.name}</div>
+            <SchoolList ref={scrollRef} style={{height: `${containerHeight}px`}}>
+              <div style={{ transform : `translateY(${translateY}px)` }}>
+                {visibleItem?.map((ele, idx) => (
+                    <div
+                        key={idx}
+                        style={{
+                          height: `${itemHeight}px`,
+                          marginTop: `${columnGap}px`,
+                          background: "green",
+                        }}
+                    >
+                      {ele.name}
+                    </div>
                 ))}
-              </SchoolList>
-            </>
+              </div>
+            </SchoolList>
 
         )}
       </div>
   );
 };
 const SchoolList = styled.div`
-    height: 500px;
-    overflow: scroll;
     > div {
-        height: 200px;
-        background: green;
-        margin-top: 10px;
+        height: auto;
     }
 `
 export default SchoolSelector;
